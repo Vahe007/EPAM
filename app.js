5381a027473c0de4061a8bbd6bf3a513b441c1ad
Vahe Manukyan <vahemanukyan@Vahes-MacBook-Air-2.local>
2023-06-01 21:40:08 +0400

    completed

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ function deepestDirectory(path, depth) {
         if (file.isDirectory()) {
             const newPath = path + `/${file.name}`
             const newDepth = depth + 1
-            if (maxDepth < depth) {
-                maxDepth = depth
-                deepestDir = path
+            if (maxDepth < newDepth) {
+                maxDepth = newDepth
+                deepestDir = newPath
             }
             deepestDirectory(newPath, newDepth)
         }
@@ -21,4 +21,7 @@ deepestDirectory(process.cwd(), 0)
 
 fs.appendFile(`${deepestDir}/file.txt`, 'hello world', (err) => {
     if (err) console.log(err.message)
+    else {
+        console.log('file.txt is created inside the ' + deepestDir)
+    }
 })
